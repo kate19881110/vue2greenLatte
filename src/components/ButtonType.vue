06e942874e8ae4e7f2e25b1c717f046584f6c542
KateLiu <KateLiu@wistronits.com>
2023-03-28 14:57:35 +0800

    add buttonType component

---
+++ src/components/ButtonType.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <button
+    class="common_style"
+    :class="[text, textHover]"
+    @click="$emit('click')"
+  >
+    <slot />
+  </button>
+</template>
+<script>
+export default {
+  name: "ButtonType",
+  props: {
+    text: {
+      default: "textBtn",
+      type: String,
+    },
+    textHover: {
+      default: "textBtn:hover",
+      type: String,
+    },
+  },
+};
+</script>
+<style lang="sass" scoped>
+@import @/styles/common.sass
+
+.common_style
+  @extend .common_button
+
+.textBtn
+  @extend .button_text
+
+.textBtn:hover
+  @extend .button_text_hover
+</style>
